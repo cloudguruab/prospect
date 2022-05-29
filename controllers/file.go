078ae85a5f109cd9cbfe6515f9fa632fac38d59e
cloudguruab <ap.brown011@gmail.com>
2022-05-28 21:50:56 -0400

    dev: bucket setup

--- controllers/file.go
+++ controllers/file.go
@@ -1,10 +1,30 @@
 package controllers
 
 import (
-    "fmt"
+        "context"
+        "fmt"
+        "log"
+        "time"
+
+        "github.com/cloudguruab/prospect/common"
+        "cloud.google.com/go/functions/metadata"
 )
 
 
-func Parser() {
-    fmt.Println("This is the file controller that will handle file objects")
+func TriggerGCS(ctx context.Context, e common.GCSEvent) error {
+        meta, err := metadata.FromContext(ctx)
+
+        if err != nil {
+                return fmt.Errorf("metadata.FromContext: %v", err)
+        }
+
+        log.Printf("Event ID: %v\n", meta.EventID)
+        log.Printf("Event type: %v\n", meta.EventType)
+        log.Printf("Bucket: %v\n", e.Bucket)
+        log.Printf("File: %v\n", e.Name)
+        log.Printf("Metageneration: %v\n", e.Metageneration)
+        log.Printf("Created: %v\n", e.TimeCreated)
+        log.Printf("Updated: %v\n", e.Updated)
+
+        return nil
 }
